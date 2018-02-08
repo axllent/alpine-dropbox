9f05f9fd60760025e0f761749bb517ea966e9fcf
Ralph Slooten <axllent@gmail.com>
2018-02-09 08:57:37 +1300

    Update script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -d ~/.dropbox-dist ]; then
-    echo "Dropbox client not found, downloading... please wait"
+    echo "Dropbox client not found, downloading... please wait..."
     wget -q -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
     echo "Installed Dropbox version:" $(cat /dbox/.dropbox-dist/VERSION)
 fi
